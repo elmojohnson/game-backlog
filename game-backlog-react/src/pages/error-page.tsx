e1a825465fd326a600f5bf219c21f654195654d8
John Elmo Johnson <elmoj6@gmail.com>
2025-04-22 13:28:37 +1000

    Updated UI

--- game-backlog-react/src/pages/error-page.tsx
+++ game-backlog-react/src/pages/error-page.tsx
@@ -1,8 +1,17 @@
-export default function ErrorPage({ error }: { error?: Error }) {
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
+
+export default function ErrorPage() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>There was an error. Please try again.</h1>
-      <p>{error?.message}</p>
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="flex flex-col gap-4 items-center">
+        <h1 className="font-bold text-5xl">There was an error. Please try again.</h1>
+        <Button variant="outline" onClick={() => navigate(-1)}>
+          Go back
+        </Button>
+      </div>
     </div>
   );
 }
