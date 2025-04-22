767547ae96d983c3c79f214a803cd1c5c5da4a06
John Elmo Johnson <elmoj6@gmail.com>
2025-04-22 13:12:29 +1000

    Update view-backlog.tsx

--- game-backlog-react/src/pages/backlogs/view-backlog.tsx
+++ game-backlog-react/src/pages/backlogs/view-backlog.tsx
@@ -4,37 +4,25 @@ import useBacklog from "@/hooks/useBacklog";
 import { Edit, Trash2Icon } from "lucide-react";
 import { Link, Navigate, Outlet, useLocation, useParams } from "react-router";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 
 export default function ViewBacklog() {
   let { id } = useParams();
   const location = useLocation();
-  const [currentTab, setCurrentTab] = useState<string | null>(null);
   const { getBacklogById, updateBacklog, deleteBacklog } = useBacklog();
   const { data: backlog, status } = getBacklogById(parseInt(id!));
 
   const tabs = [
     {
       title: "Backlog",
-      value: "backlog",
       href: `/backlogs/${id}`,
     },
     {
       title: "Browse",
-      value: "browse",
       href: `/backlogs/${id}/browse-games`,
     },
   ];
-
-  useEffect(() => {
-    if (location.pathname === `/backlogs/${id}`) {
-      setCurrentTab("backlog");
-    } else if (location.pathname === `/backlogs/${id}/browse-games`) {
-      setCurrentTab("browse");
-    }
-  }, [location.pathname]);
-
+  
   if (status === "pending") {
     return <Loading />;
   }
@@ -64,17 +52,17 @@ export default function ViewBacklog() {
         </div>
       </div>
 
-      <Tabs defaultValue={currentTab!} className="w-full">
-        <TabsList>
+      <Tabs defaultValue={location.pathname} className="w-full">
+        <TabsList className="w-full">
           {tabs.map((tab) => {
             return (
-              <TabsTrigger key={tab.value} value={tab.value} asChild>
+              <TabsTrigger key={tab.title} value={tab.href} asChild>
                 <Link to={tab.href}>{tab.title}</Link>
               </TabsTrigger>
             );
           })}
         </TabsList>
-        <TabsContent value={currentTab!}>
+        <TabsContent value={location.pathname}>
           <Outlet />
         </TabsContent>
       </Tabs>
