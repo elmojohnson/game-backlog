77e14efd7920033d1ba433b09c3b8606dcad9348
John Elmo Johnson <elmoj6@gmail.com>
2025-05-03 21:12:47 +1000

    Update main.tsx

--- game-backlog-react/src/main.tsx
+++ game-backlog-react/src/main.tsx
@@ -58,7 +58,7 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-// Test 123
+// Test 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
     <QueryClientProvider client={queryClient}>
